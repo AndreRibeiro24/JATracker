c4c6908f0070f9f912d5c7f4c4d7d6325b992fa3
AndreRibeiro24 <andrebecas20@gmail.com>
2026-06-09 19:43:04 +0100

    hero image for HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import Footer from "../components/Footer";
+import HeroSlider from "../components/HeroSlider";
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
         <Link to="/about-us">
           <p>about us page</p>
         </Link>
+        <HeroSlider/>
       </div>
       <Footer />
     </div>
