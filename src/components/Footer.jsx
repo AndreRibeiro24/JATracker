84e8790c6d888fab7b6d1897910837a51389697c
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-06-09 17:40:00 +0200

    Style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ import {
 
 function Footer() {
   return (
-    <main className="border bg-[#0b1326] h-[45vh] font-mono">
+    <main className="border bg-[#0b1326] h-110 font-mono">
       <section className="flex items-center justify-center gap-2 my-[2%] border-2 border-[#00687A] w-80 ml-12.5 p-1 rounded-4xl">
         <div className="bg-[#00687A] aspect-square h-3 w-3 rounded-full"></div>
         <h2 className="text-[#00687A] font-extrabold text-md">
@@ -66,16 +66,16 @@ function Footer() {
             "linear-gradient(to right, transparent, #00687A, transparent)",
         }}
       ></div>
-      <section>
-        <aside>
+      <section className="flex justify-center gap-[35%] items-center mt-5">
+        <aside className="flex gap-2">
           <FaRegCopyright className="text-[#ffffff] w-3 h-3" />
-          <small className="text-[#ffffff] text-">
+          <small className="text-[#ffffff] text-[0.6rem]">
             2026 JATracker Gaming Platform. All rights reserved
           </small>
         </aside>
         <aside>
-          <small className="text-[#ffffff]">Privacy Policy</small>
-          <small className="text-[#ffffff] text-[0.5rem]">
+          <small className="text-[#ffffff] text-[0.6rem]">Privacy Policy</small>
+          <small className="text-[#ffffff] text-[0.6rem]">
             Terms of service
           </small>
         </aside>
