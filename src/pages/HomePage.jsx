0a15589ca8b0aa05df029930753d82527e529e93
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-06-10 17:16:59 +0200

    Check

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,7 @@
 import { Link } from "react-router";
 import Footer from "../components/Footer";
-<<<<<<< HEAD
 import GameList from "../components/GameList";
-=======
 import HeroSlider from "../components/HeroSlider";
->>>>>>> 38c030dcfdb5be6e7230c18dacd47a5981fa9ead
 
 export default function HomePage() {
   return (
@@ -14,7 +11,7 @@ export default function HomePage() {
         <Link to="/about-us">
           <p>about us page</p>
         </Link>
-        <HeroSlider/>
+        <HeroSlider />
       </div>
       <GameList />
       <Footer />
