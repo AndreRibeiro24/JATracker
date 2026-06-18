6c95839088d3255e749739e76d501ebdc1638190
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-06-18 20:58:46 +0200

    Error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,318 @@
 import React from "react";
+import { GoDot } from "react-icons/go";
+import { FaSpaghettiMonsterFlying } from "react-icons/fa6";
+import { ImPacman } from "react-icons/im";
+import { Link } from "react-router-dom";
+import { BiSolidLeftArrow } from "react-icons/bi";
 
 function Error() {
-  return <div>Error</div>;
+  return (
+    <main className="w-[90%] mx-auto mt-20">
+      <section className="flex justify-between">
+        <h1 className="text-amber-50 text-[4rem]">404</h1>
+        <Link to="/">
+          <button className="text-amber-50 border-6 border-emerald-600 text-[2rem] px-7.5 cursor-pointer">
+            GO BACK
+          </button>
+        </Link>
+      </section>
+      <section className="mt-10">
+        <div className="h-5 bg-emerald-600"></div>
+        <article>
+          <div className="flex">
+            <div className="h-20 w-30 flex justify-center items-center border-l-20 border-emerald-600 border-r-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <FaSpaghettiMonsterFlying className="text-amber-50 h-15 w-15" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <FaSpaghettiMonsterFlying className="text-amber-50 h-15 w-15" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-20 flex justify-center items-center">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-20 w-30 flex justify-center items-center border-r-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+          </div>
+          <div className="flex border-l-20 border-emerald-600 ">
+            <div className="h-25 w-17.5 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <FaSpaghettiMonsterFlying className="text-amber-50 h-15 w-15" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-25 flex justify-center items-center border-t-20 border-emerald-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-25 flex justify-center items-center border-t-20 border-emerald-600 border-r-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+          </div>
+          <div className="flex border-l-20 border-red-600 border-r-20">
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center order-r-20 border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-r-20 border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <FaSpaghettiMonsterFlying className="text-amber-50 h-15 w-15" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-t-20 border-l-20">
+              <FaSpaghettiMonsterFlying className="text-amber-50 h-25 w-25" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-r-20 border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-b-20 border-l-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-25 flex justify-center items-center border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+          </div>
+          <div className="flex border-l-20 border-emerald-600 w-full">
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-16 flex justify-center items-center border-r-20 border-e-red-600">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-19 flex justify-center items-center border-red-600 border-r-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-29 flex justify-center items-center border-red-600 border-r-20 border-b-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-20">
+              <FaSpaghettiMonsterFlying className="text-amber-50 h-15 w-15" />
+            </div>
+            <div className="h-30 w-20 flex justify-center items-center border-red-600 border-r-20">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+          </div>
+          <div className="flex border-l-20 border-emerald-600 w-full border-r-20 border-b-20">
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+            <div className="h-25 w-20 flex justify-center items-center border-[rgba(0,0,0,0)] border-t-9">
+              <GoDot className="text-amber-50 bg-amber-50 border rounded-full" />
+            </div>
+          </div>
+          <div className="mt-5 mx-auto flex justify-between">
+            <div className="flex gap-5">
+              <ImPacman className="h-10 w-10 text-amber-400" />
+              <ImPacman className="h-10 w-10 text-amber-400" />
+              <ImPacman className="h-10 w-10 text-amber-400" />
+            </div>
+            <div>
+              <h2 className="font-mono text-[2rem] text-amber-50">
+                Score: 100
+              </h2>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2 my-10">
+            <div className="h-20 w-20 bg-gray-500 rounded-t-xl flex justify-center items-center">
+              <BiSolidLeftArrow className="w-10 h-10 rotate-90 text-[#00687A]" />
+            </div>
+            <div className="flex gap-2">
+              <div className="h-20 w-20 bg-gray-500 rounded-l-xl flex justify-center items-center">
+                <BiSolidLeftArrow className="w-10 h-10 text-[#00687A]" />
+              </div>
+              <div className="h-20 w-20 bg-gray-500 flex justify-center items-center">
+                <BiSolidLeftArrow className="w-10 h-10 rotate-270 text-[#00687A]" />
+              </div>
+              <div className="h-20 w-20 bg-gray-500 rounded-r-xl flex justify-center items-center">
+                <BiSolidLeftArrow className="w-10 h-10 rotate-180 text-[#00687A]" />
+              </div>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default Error;
